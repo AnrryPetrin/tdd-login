7e8b6b5414f5d332bc0e17d8c82dd5126c4c8cde
AnrryPetrin <108158140+AnrryPetrin@users.noreply.github.com>
2024-05-14 23:43:42 -0300

    fixes

--- src/main/java/com/qa/LoginScreen.java
+++ src/main/java/com/qa/LoginScreen.java
@@ -1,17 +1,54 @@
 package com.qa;
 
-import java.util.function.BooleanSupplier;
+import java.util.HashMap;
+import java.util.Map;
 
+/*
+ * This class is a simple login screen that allows users to login and add new users.    
+ */
 public class LoginScreen {
+    private Map<String, String> userDatabase;
 
-    public BooleanSupplier login(String string, String string2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'login'");
+    /*
+     * Constructor that initializes the userDatabase with two default users.
+     */
+    public LoginScreen() {
+        userDatabase = new HashMap<>();
+        userDatabase.put("admin", "admin");
+        userDatabase.put("user", "user");
     }
 
-    public void addUser(String string, String string2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addUser'");
+    /*
+     * Method that checks if the username and password are correct.
+     * 
+     * @param username The username of the user.
+     * 
+     * @param password The password of the user.
+     * 
+     * @return True if the username and password are correct, false otherwise.
+     */
+    public boolean login(String username, String password) {
+        if (userDatabase.containsKey(username)) {
+            String storedPassword = userDatabase.get(username);
+            if (storedPassword.equals(password)) {
+                return true;
+            } else {
+                return false;
+
+            }
+        }
+        return false;
+    }
+
+    /*
+     * Method that adds a new user to the userDatabase.
+     * 
+     * @param username The username of the new user.
+     * 
+     * @param password The password of the new user.
+     */
+    public void addUser(String username, String password) {
+        userDatabase.put(username, password);
     }
 
 }
--- src/test/java/LoginScreenTest.java
+++ src/test/java/LoginScreenTest.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class LoginScreenTest {
+import org.junit.Test;
+
+import com.qa.LoginScreen;
+
+import static org.junit.Assert.*;
+
+public class LoginScreenTest {
+    @Test
+    public void testSuccessfulLogin() {
+        LoginScreen loginScreen = new LoginScreen();
+        assertTrue(loginScreen.login("admin", "admin"));
+    }
+
+    @Test
+    public void testFailedLogin() {
+        LoginScreen loginScreen = new LoginScreen();
+        assertFalse(loginScreen.login("admin", "wrongpassword"));
+    }
+
+    @Test
+    public void testAddUser() {
+        LoginScreen loginScreen = new LoginScreen();
+        loginScreen.addUser("newuser", "password");
+    }
 }
