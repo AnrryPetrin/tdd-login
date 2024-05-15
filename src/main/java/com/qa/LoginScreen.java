7834d2f33915dc55fe3581fc2dfe1aa09bc01b38
AnrryPetrin <108158140+AnrryPetrin@users.noreply.github.com>
2024-05-14 23:43:28 -0300

    initial commit

---
+++ src/main/java/com/qa/LoginScreen.java
@@ -0,0 +1,17 @@
+package com.qa;
+
+import java.util.function.BooleanSupplier;
+
+public class LoginScreen {
+
+    public BooleanSupplier login(String string, String string2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'login'");
+    }
+
+    public void addUser(String string, String string2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addUser'");
+    }
+
+}
